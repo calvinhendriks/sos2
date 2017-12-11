c5f1e0c3012eb5033b5cb21df48149091f056ee6
calvinhendriks <calvinhendriks@hotmail.com>
2017-12-11 16:53:27 +0100

    total words and most used words impolement

--- part B/textstat/src/main.rs
+++ part B/textstat/src/main.rs
@@ -3,6 +3,7 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 use std::env;
+use std::collections::HashMap;
 
 fn main() {
 
@@ -29,6 +30,45 @@ fn main() {
         Err(why) => panic!("couldn't read {}: {}", display,
                                                    why.description()),
         Ok(_) => print!("{} contains:\n{}", display, s),
+    }
+
+    s.replace(".", "");
+    s.replace(",","");
+    s.remove(".");
+
+
+
+    let mut words: HashMap<&str, usize> = HashMap::new();
+    for word in s.split(is_whitespace).filter(is_not_empty) {
+        if let Some(count) = words.get_mut(word) {
+            *count += 1;
+            continue;
+        }
+        words.insert(word, 1);
+    }
+
+
+
+    let iterator = words.iter();
+    let counter = iterator.count();
+
+    let iterator2 = words.iter();
+    let mut count_vec: Vec<_> = iterator2.collect();
+    count_vec.sort_by(|a, b| b.1.cmp(&a.1));
+    count_vec.truncate(10);
+
+
+    println!("{}", counter);
+    println!("{:?}",count_vec);
+
+    #[inline]
+    fn is_not_empty(s: &&str) -> bool {
+        !s.is_empty()
+    }
+
+    #[inline]
+    fn is_whitespace(c: char) -> bool {
+        c == ' ' || c == '\t' || c == '\n'
     }
 
     // `file` goes out of scope, and the "hello.txt" file gets closed
